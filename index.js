c9f10b257a2ee14d586696450dd9d0fee49077ca
krunal5291 <krunalkalathiya11@gmail.com>
2024-03-12 09:01:14 +0530

    ok

--- index.js
+++ index.js
@@ -1,24 +1,40 @@
-class employ{
-    constructor(name,number ,salary,post){
-        this.name=name;
-        this.number=number;
-        this.salary=salary;
-        this.post=post;
+class employ {
+    constructor(name, number, salary, post) {
+        this.name = name;
+        this.number = number;
+        this.salary = salary;
+        this.post = post;
     }
 
-    name(){
+    name() {
         console.log(`${this.name} this is the name`);
     }
 
-    number(){
-       console.log(`${this.number} this is your number`); 
+    number() {
+        console.log(`${this.number} this is your number`);
     }
 
 }
 
-let krunal=new employ(krunal,7984433994,10000000000000000000000,MD);
+class manager extends employ {
+    constructor(name, number, salary, post, boss) {
+        super(name, number, salary, post);
+        this.boss = boss;
+    }
+
+    b() {
+        if (this.boss == true) {
+            console.log("good");
+        }
+        else {
+            console.log("sorry");
+        }
+    }
+}
+
+let k = new manager("krunal", 7984433994, 100000, "MD", true);
 
 
-console.log(krunal);
+console.log(k);
 
-krunal.name();
+k.manager.b();
